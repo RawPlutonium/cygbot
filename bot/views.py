fe30f1b575d82157952f6ca014f2300cfb222cff
rawplutonium <lgithigi@gmail.com>
2017-08-20 21:48:54 +0300

    M

--- bot/views.py
+++ bot/views.py
@@ -7,4 +7,7 @@ from django.http.response import HttpResponse
 # Create your views here.
 PAT = "EAALvgVZA7qVcBAEBZBOPROoZAhVJANIFOik1OJJ8nXgeBD2OVvtM0tDKgphu4n6VUFLPZAUHovjOgnFu5uPO8wLRZCg2g2P6ZByrm10XKy2eZCIO3rrzFv7OMQAIAl37rIqOu8hWlfjDzro8RQADWlE6goFXXrE45ZBm6xbw4ZCqECAZDZD"
 def bview(request):
-    return HttpResponse("Hello World!")
+     if request.GET['hub.verify_token'] == '90293269':
+        return HttpResponse(request.GET['hub.challenge'])
+     else :
+        return HttpResponse('Invalid Token !')
