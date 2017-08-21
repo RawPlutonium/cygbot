7a3536cf643295fb8470d6949440cb0de803af9c
rawplutonium <lgithigi@gmail.com>
2017-08-21 16:02:59 +0300

    B

--- bot/views.py
+++ bot/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http.response import HttpResponse
 import json
+import pprint
 
 # Create your views here.
 PAT = "EAALvgVZA7qVcBAEBZBOPROoZAhVJANIFOik1OJJ8nXgeBD2OVvtM0tDKgphu4n6VUFLPZAUHovjOgnFu5uPO8wLRZCg2g2P6ZByrm10XKy2eZCIO3rrzFv7OMQAIAl37rIqOu8hWlfjDzro8RQADWlE6goFXXrE45ZBm6xbw4ZCqECAZDZD"
